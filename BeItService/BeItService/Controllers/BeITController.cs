49e2a1a4b7b7bbdbc438ac5e08ba3e8c1b73e158
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-02-26 12:29:41 +0100

    Initial Commit

---
+++ BeItService/BeItService/Controllers/BeITController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeItService.Controllers
+{
+    public class BeITController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
